b9a02008172f423494dc65fcf8827c007604d067
Claudia Pierson <c172@umbc.edu>
2020-02-07 05:12:38 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,31 +1,28 @@
 require 'pry'
-
-
-class String
-
+class String 
   def sentence?
-    if self.end_with?(".")
-      return true
-    else
-      return false
-    end
+    if self.end_with? (".")
+      return true 
+  else
+    return false 
   end
+end 
 
-  def question?
-    if self.end_with?("?")
-      return true
-    else
-      return false
-    end
-  end
+def question?
+  if self.end_with? ("?")
+    return true
+  else
+    return false
+  end 
+end 
 
-  def exclamation?
-    if self.end_with?("!")
-      return true
-    else
-      return false
-    end
-  end
+def exclamation?
+  if self.end_with ("!")
+    return true
+  else
+    return false 
+  end 
+end 
 
   def count_sentences
     new_array = self.split(/[.?!]/).reject {|string| string.empty?}
